3d8d5433edada88841fe650cea10995d5ad20ba7
Vanessa Vieira <vanessafvieira22@gmail.com>
2023-02-23 10:21:17 +0000

    updated

--- models/User.model.js
+++ models/User.model.js
@@ -25,16 +25,10 @@ const userSchema = new Schema(
     },
     friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
     image: {
-<<<<<<< HEAD
       type:String,
       default:"https://res.cloudinary.com/dmm8iusle/image/upload/v1677086060/istockphoto-1125089587-170667a_gh0zbo.jpg",
     }
-=======
-      type: String,
-      default:
-        "https://as2.ftcdn.net/v2/jpg/03/03/62/45/1000_F_303624505_u0bFT1Rnoj8CMUSs8wMCwoKlnWlh5Jiq.jpg",
-    },
->>>>>>> 0243775cfca9b2acc6d4d570373df555b80aeb97
+  
   },
   {
     timestamps: true,
